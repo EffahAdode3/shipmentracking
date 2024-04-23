5bd3575768882f9fc2093eb685e828d8064e0e07
EffahAdode3 <whatyousow00@gmail.com>
2024-04-23 14:13:19 -0700

    change in constant

--- vue-project/src/constants.js
+++ vue-project/src/constants.js
@@ -1,3 +1,3 @@
-export const base_url="https://systemtracking.vercel.app"
-
 // export const base_url="https://systemtracking.vercel.app"
+
+export const base_url="https://systemtracking-bb4hwlfrg-effahadode3s-projects.vercel.app"
